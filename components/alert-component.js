f02297e910c26ff940a490c4e9f6bb8d713c4240
Joy <joy.cheng97324@gmail.com>
2021-11-15 13:40:47 +0800

    edit

--- components/alert-component.js
+++ components/alert-component.js
@@ -1,10 +1,10 @@
 export default {
   data() {
     return {
-      text: '這是模組化元件'
+      text: 'export default 外部匯入元件'
     };
   },
   template: `<div class="alert alert-primary" role="alert">
-      {{ text }}
-    </div>`
-}
+    {{ text }}
+  </div>`
+};
